cfec5c8cb09241e97a3ace55c8b3b100d10c1e36
Ackin <ackinlino123@gmail.com>
2024-06-15 13:56:24 -0300

    alteração

--- database/cliente.py
+++ database/cliente.py
@@ -2,4 +2,7 @@ CLIENTES = [
     {"id": 1, "nome": "Arthur", "email": "iasuhdaisudh@gmff.com"},
     {"id": 2, "nome": "Arthur", "email": "iasuhdaisudh@gmff.com"},
     {"id": 3, "nome": "Arthur", "email": "iasuhdaisudh@gmff.com"},
+    {"id": 1, "nome": "Arthur", "email": "iasuhdaisudh@gmff.com"},
+    {"id": 2, "nome": "Arthur", "email": "iasuhdaisudh@gmff.com"},
+    {"id": 3, "nome": "Arthur", "email": "iasuhdaisudh@gmff.com"},
 ]
